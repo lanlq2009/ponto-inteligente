f8c60339cc62e1c9cfb0f82cce4268d8c47ecf50
lanlq2009 <lanlq2009@gmail.com>
2021-11-15 23:22:23 -0300

    Criando componente de cadastro de PJ

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CadastroPJ } from '../../models';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPjComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBarModule,
+    private router: Router
+  ) {  }
 
   ngOnInit(): void {
+     this.gerarForm();
+  }
+
+  //Mapea o formulario com o componente alem de adicionar validadores.
+  gerarForm(){
+        this.form = this.fb.group({
+          nome: ['', [Validators.required, Validators.minLength(3)]],
+          email: ['',[Validators.required, Validators.email] ],
+          senha:['', [Validators.required, Validators.minLength(6)]],
+          cpf:['', [Validators.required]],
+          razaoSocial:['', [Validators.required]],
+          cnpj:['', Validators.required]  
+        });
+  }
+
+  cadastrarPJ(){
+    if(this.form.invalid){
+      return;
+    }
+    const cadastroPJ: CadastroPJ = this.form.value;
+    alert(JSON.stringify(cadastroPJ));
+
   }
 
 }
